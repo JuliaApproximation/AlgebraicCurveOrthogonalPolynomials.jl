602511a592857b892ca4cf5d7d23f218a045d76b
Sheehan Olver <solver@mac.com>
2020-04-07 22:05:27 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,20 @@
 using OrthogonalPolynomialsAlgebraicCurves, BlockBandedMatrices, BlockArrays, FillArrays, Test
 
+@testset "Quartic" begin
+    @testset "Commutating properties" begin
+        X,Y = quarticjacobi(10)
+        @test (X*Y)[Block.(1:11), Block.(1:11)] ≈ (Y*X)[Block.(1:11), Block.(1:11)]
+        @test (X^4 + Y^4)[Block.(1:10), Block.(1:10)] ≈ Eye(34)
+    end
 
-X,Y = quarticjacobi(10)
-@test (X*Y)[Block.(1:11), Block.(1:11)] ≈ (Y*X)[Block.(1:11), Block.(1:11)]
-@test (X^4 + Y^4)[Block.(1:10), Block.(1:10)] ≈ Eye(34)
+    @testset "Toeplitz SVD" begin
+        X,Y = quarticjacobi(30)
+        K = 25; σ1 = svdvals(Float64.(X[Block(K,K+1)]))
+        K = 26; σ2 = svdvals(Float64.(X[Block(K,K+1)]))
+        @test σ1 ≈ σ2 rtol=1E-2
 
-
-X,Y = quarticjacobi(30)
-K = 25; σ1 = svdvals(Float64.(X[Block(K,K+1)]))
-K = 26; σ2 = svdvals(Float64.(X[Block(K,K+1)]))
-@test σ1 ≈ σ2 rtol=1E-2
-
-K = 25; σ1 = svdvals(Float64.(Y[Block(K,K+1)]))
-K = 26; σ2 = svdvals(Float64.(Y[Block(K,K+1)]))
-@test σ1 ≈ σ2 rtol=1E-2
+        K = 25; σ1 = svdvals(Float64.(Y[Block(K,K+1)]))
+        K = 26; σ2 = svdvals(Float64.(Y[Block(K,K+1)]))
+        @test σ1 ≈ σ2 rtol=1E-2
+    end
+end
