0e5e17b35a9508b8ddceb0b91c8d77a943e00a3a
MarcoFasondini <60903315+MarcoFasondini@users.noreply.github.com>
2020-04-14 00:03:30 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using OrthogonalPolynomialsAlgebraicCurves, BlockBandedMatrices, BlockArrays, FillArrays, Test
+using OrthogonalPolynomialsAlgebraicCurves, LinearAlgebra, BlockBandedMatrices, BlockArrays, FillArrays, Test
 
 @testset "Quartic" begin
     @testset "Commutating properties" begin
@@ -16,5 +16,31 @@ using OrthogonalPolynomialsAlgebraicCurves, BlockBandedMatrices, BlockArrays, Fi
         K = 25; σ1 = svdvals(Float64.(Y[Block(K,K+1)]))
         K = 26; σ2 = svdvals(Float64.(Y[Block(K,K+1)]))
         @test σ1 ≈ σ2 rtol=1E-2
+    end
+    
+    @testset "32-by-32 symbols" begin
+        periods = 5
+        n=8*periods+7
+        X,Y=quarticjacobi(n)
+        Ax = BlockBandedMatrix(zeros(BigFloat,32,32), fill(4,8),fill(4,8), (1,1))
+        for b = 0:6
+            Ax[Block(b+1,b+2)]=X[Block(n-6+b,n-5+b)]
+            Ax[Block(b+2,b+1)]=X[Block(n-5+b,n-6+b)]
+        end
+        Bx=BlockArray{BigFloat}(zeros(BigFloat,32,32), fill(4,8), fill(4,8))
+        Bx[Block(8,1)]=X[Block(n+1,n+2)]
+        Ay = BlockBandedMatrix(zeros(BigFloat,32,32), fill(4,8),fill(4,8), (1,1))
+        for b = 0:6
+            Ay[Block(b+1,b+2)]=Y[Block(n-6+b,n-5+b)]
+            Ay[Block(b+2,b+1)]=Y[Block(n-5+b,n-6+b)]
+        end
+        By=BlockArray{BigFloat}(zeros(BigFloat,32,32), fill(4,8), fill(4,8))
+        By[Block(8,1)]=Y[Block(n+1,n+2)]
+        θ=pi/2;
+        z=exp(im*θ)
+        xz = Bx'/z+Ax+Bx*z
+        yz = By'/z+Ay+By*z
+        @test xz*yz ≈ yz*xz rtol = 1E-2
+        @test xz^4+yz^4≈I rtol=1E-2
     end
 end
