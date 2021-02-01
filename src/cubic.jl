e94d8ab2cdd0d14a0687c859f873859681a540de
Sheehan Olver <solver@mac.com>
2021-02-01 16:59:54 +0000

    work on cubic OPs

--- src/cubic.jl
+++ src/cubic.jl
@@ -55,10 +55,10 @@ function getindex(P::LegendreCubic{T}, xy::StaticVector{2}, J::Block{1}) where T
     J == Block(1) && return [one(T)]
     J == Block(2) && return [P.P[x,2], y*P.Q[x,1]]
     J̃ = Int(J)
-    m = J̃ ÷ 2
-    isodd(J̃) && return [P.P[x,3m+1], y*P.Q[x, 3m-1], P.P[x, 3m]]
+    m = 3J̃ ÷ 2
+    isodd(J̃) && return [P.P[x,m-1], y*P.Q[x, m-2], P.P[x, m]]
     #iseven(J̃)
-    return [y*P.Q[x, 3m-2], P.P[x,3m-1], y*P.Q[x, 3m-3]]
+    return [y*P.Q[x, m-3], P.P[x,m-1], y*P.Q[x, m-2]]
 end
 
 function getindex(P::LegendreCubic{T}, xy::StaticVector{2}, Jj::BlockIndex{1}) where T
@@ -75,3 +75,74 @@ function getindex(P::LegendreCubic, xy::StaticVector{2}, j::Int)
 end
 
 summary(io::IO, P::LegendreCubic{V}) where V = print(io, "LegendreCubic($(P.t))")
+
+abstract type AbstractLegendreCubicJacobi{T} <: AbstractBlockBandedMatrix{T} end
+Base.copy(J::AbstractLegendreCubicJacobi) = J # immutable
+
+axes(::AbstractLegendreCubicJacobi) = (_BlockedUnitRange(Vcat(1, 3:3:∞)), _BlockedUnitRange(Vcat(1, 3:3:∞)))
+
+function getindex(X::AbstractLegendreCubicJacobi, k::Int, j::Int)
+    ki,ji = findblockindex.(axes(X), (k,j))
+    X[block(ki),block(ji)][blockindex(ki),blockindex(ji)]
+end
+
+
+struct LegendreCubicJacobiX{T} <: AbstractLegendreCubicJacobi{T}
+    X_P
+    X_Q
+end
+
+LegendreCubicJacobiX(X_P, X_Q) = LegendreCubicJacobiX{promote_type(eltype(X_P),eltype(X_Q))}(X_P,X_Q)
+
+
+function BlockArrays.viewblock(X::LegendreCubicJacobiX{T}, kj::Block{2}) where T
+    k,j = kj.n
+    X_P, X_Q = X.X_P, X.X_Q
+    k == j == 1 && return reshape([X_P[1,1]],1,1)
+    (k,j) == (2,1) && return reshape([X_P[2,1],0], 2, 1)
+    (k,j) == (1,2) && return [X_P[1,2] 0]
+    k == 1 && return zeros(T,1,3)
+    j == 1 && return zeros(T,3,1)
+    k == j == 2 && return [X_P[2,2] 0; 0 X_Q[1,1]]
+    (k,j) == (3,2) && return [X_P[3,2] 0; 0 X_Q[2,1]; 0 0]
+    (k,j) == (2,3) && return [X_P[2,3] 0 0; 0 X_Q[2,1] 0]
+    k == 2 && return zeros(T,2,3)
+    j == 2 && return zeros(T,3,2)
+    if k == j
+        m = 3k ÷ 2
+        if isodd(k) 
+            return [X_P[m-1,m-1] 0              X_P[m-1,m]; 
+                    0            X_Q[m-2,m-2]   0       ;
+                    X_P[m,m-1] 0              X_P[m,m]]
+        else
+            return [X_Q[m-3,m-3] 0              X_Q[m-3,m-2]; 
+                    0            X_P[m-1,m-1]   0       ;
+                    X_Q[m-2,m-3] 0              X_Q[m-2,m-2]]
+        end
+    elseif k == j+1
+        m = 3j ÷ 2
+        if isodd(k) 
+            return [0        X_Q[m-1,m-2]             0; 
+                    0           0                   X_P[m+1,m];
+                    0           0                     0]
+        else
+            return [0        X_P[m,m-1]             0; 
+                    0           0                   X_Q[m-1,m-2];
+                    0           0                     0]
+        end
+    elseif k+1 == j
+        m = 3k ÷ 2
+        if isodd(j) 
+            return [0           0             0; 
+                    X_Q[m-1,m-2]           0                   0;
+                    0           X_P[m+1,m]                     0]
+        else
+            return [0        0             0; 
+                    X_P[m,m-1]           0                   0;
+                    0           X_Q[m-1,m-2]                     0]
+        end
+    else
+        zeros(T, 3, 3)
+    end
+end
+
--- test/test_cubic.jl
+++ test/test_cubic.jl
@@ -1,4 +1,6 @@
-using OrthogonalPolynomialsAlgebraicCurves, ClassicalOrthogonalPolynomials, Test
+using OrthogonalPolynomialsAlgebraicCurves, ClassicalOrthogonalPolynomials, FillArrays, BlockArrays, LazyBandedMatrices, LazyArrays, Test
+import LazyBandedMatrices: BlockHvcat
+import OrthogonalPolynomialsAlgebraicCurves: LegendreCubicJacobiX
 
 @testset "cubic" begin
     P = LegendreCubic(2)
@@ -7,7 +9,27 @@ using OrthogonalPolynomialsAlgebraicCurves, ClassicalOrthogonalPolynomials, Test
     xy = SVector(x, sqrt(x*(1-x)*(P.t-x)))
     
     @testset "jacobi" begin
-        
+        X_P = jacobimatrix(P.P);
+        X_Q = jacobimatrix(P.Q);
+
+        X = LegendreCubicJacobiX(X_P, X_Q)
+
+        N = 3
+        x*P[xy,Block.(1:N)]'
+        P[xy,Block.(1:N+1)]' * X[Block.(1:N+1), Block.(1:N)]
+
+        # @testset "blocked" begin 
+        #     # make 1-blocks
+        #     X_P_bl = PseudoBlockArray(view(X_P,2:∞,2:∞), Ones{Int}(∞), Ones{Int}(∞))
+        #     X_Q_bl = PseudoBlockArray(X_Q, Ones{Int}(∞), Ones{Int}(∞))
+        #     Z = Zeros(axes(X_P_bl))
+        #     X_tail = PseudoBlockArray(BlockBroadcastArray{Float64}(hvcat, 2, X_P_bl, Z, Z, X_Q_bl),
+        #                     Vcat(2,Fill(3,∞)), Vcat(2,Fill(3,∞)));
+
+        #     r = PseudoBlockArray([X_P[2,1]; Zeros(∞)], (axes(X_tail,1),))
+
+        #     X = BlockHvcat(2, X_P[1,1], r', r, X_tail);
+        # end
     end
 end
 
